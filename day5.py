6c864c905bc24809883f698a0890ab20a1d0d44e
Alex Mueller <ajmuelle@mtu.edu>
2023-12-10 11:23:42 -0500

    day 5 part 2... really slow but still works

--- day5.py
+++ day5.py
@@ -20,7 +20,7 @@ mapFunctions = [None] * 7
 mapIndex = 0
 currentTriples = []
 
-def getMapFromTriples(triples):
+def getMapFromTriples(triples, sign=1):
     """Given a set of triples, return a function mapping x to y"""
 
     # making convenience copies partially to avoid overwriting triples
@@ -39,20 +39,28 @@ def getMapFromTriples(triples):
 
             # if x is in the correct range, apply the delta and return
             if sourceRangeStart <= x < sourceRangeStop:
-                return x + destRangeDelta
+                return x + destRangeDelta * sign
 
         # base case: x wasn't in any range, so just return it unmodified
         return x
 
     return tripleMap
 
+locationMax = 0
+
 for line in lines:
     mapName = mapNames[mapIndex]
 
     # line is just a newline
     if len(line) == 1:
-        mapFunctions[mapIndex] = getMapFromTriples(currentTriples)
+        mapFunctions[mapIndex] = getMapFromTriples(currentTriples, 1)
         mapIndex += 1
+        # for part 2
+        # if "location" in mapName:
+        #     for triple in currentTriples:
+        #         tripleRangeMax = triple[0] + triple[2]
+        #         if tripleRangeMax > locationMax:
+        #             locationMax = tripleRangeMax
 
     # line is the start of a new map
     elif mapName in line:
@@ -63,13 +71,50 @@ for line in lines:
         triple = [int(x) for x in line.split()]
         currentTriples.append(triple)
 
+def applyFunctionChain(x, chain):
+    """Apply the chain of map functions to get the seed -> location mapping"""
+    for mapFunction in chain:
+        x = mapFunction(x)
+        # print(x)
+    return x
+
+### PART ONE VERSION ###
+
 seedLocations = []
+# for seed in seeds:
+#     seedLocation = applyFunctionChain(seed, mapFunctions)
+#     seedLocations.append(seedLocation)
+#
+# print(np.min(seedLocations))
 
-# apply the chain of map functions to get the seed -> location mapping
-for seed in seeds:
-    currentValue = seed
-    for mapFunction in mapFunctions:
-        currentValue = mapFunction(currentValue)
-    seedLocations.append(currentValue)
+### PART TWO VERSION ###
 
-print(np.min(seedLocations))
+# BAD CODE, TAKES FOREVER TO RUN
+minLocation = np.inf
+numSeeds = len(seeds) // 2
+for index, value in enumerate(seeds):
+    if index % 2 == 0:
+        rangeStart = int(value)
+    else:
+        print(f"now processing seed {index // 2} of {numSeeds}")
+        for seed in range(rangeStart, rangeStart + value):
+            seedLocation = applyFunctionChain(seed, mapFunctions)
+            if seedLocation < minLocation:
+                minLocation = seedLocation
+
+print(minLocation)
+
+# BAD CODE, OUTPUTS WRONG ANSWER
+# print(f"locationMax = {locationMax}")
+# for location in range(locationMax):
+#     seed = applyFunctionChain(location, mapFunctions[::-1])
+#     for index, value in enumerate(seeds):
+#         if index % 2 == 0:
+#             rangeStart = int(value)
+#             # print(f"range start = {rangeStart}")
+#         else:
+#             if rangeStart <= seed < rangeStart + int(value):
+#                 minLocation = location
+#                 break
+#
+# print(minLocation)
