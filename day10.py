d5586f419f6eb1179a1ea4f09104820eca66acb2
Alex Mueller <ajmuelle@mtu.edu>
2023-12-11 20:53:32 -0500

    adding placeholder day 11 files (skipping day 10 for now)

--- day10.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-with open("input/day8.txt") as day8:
-    lines = day8.readlines()
-
-pass
---
+++ day11.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+with open("input/day11.txt") as day11:
+    lines = day11.readlines()
+
+pass
