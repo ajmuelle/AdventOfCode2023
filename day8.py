2629101e80afb73def3ded3f8a3c86c7a1fa8148
Alex Mueller <ajmuelle@mtu.edu>
2023-12-16 19:26:55 -0500

    solves day 8 part 2 by accident

--- day8.py
+++ day8.py
@@ -6,9 +6,11 @@ import numpy as np
 with open("input/day8.txt") as day8:
     lines = day8.readlines()
 
+leftRightStr = lines[0][:-1]
+leftRightLen = len(leftRightStr)
 # convert left/right to indices
 leftRightMap = {"L": 0, "R": 1}
-leftRightList = [leftRightMap[letter] for letter in lines[0][:-1]]
+leftRightList = [leftRightMap[letter] for letter in leftRightStr]
 
 del lines[0:2]
 
@@ -50,24 +52,60 @@ for index, line in enumerate(lines):
 
 indexMap = {}
 for index, valuePair in enumerate(valuePairs):
-    valueIndex0 = tripleToIndex[valuePair[0]]
-    valueIndex1 = tripleToIndex[valuePair[1]]
-    indexMap[index] = [valueIndex0, valueIndex1]
+    valueLeft = tripleToIndex[valuePair[0]]
+    valueRight = tripleToIndex[valuePair[1]]
+    indexMap[index] = {"L": valueLeft, "R": valueRight}
 
-def notZ(indices):
-    return indices[0] not in zIndices \
-        # or indices[1] not in zIndices \
-        # or indices[2] not in zIndices \
-        # or indices[3] not in zIndices \
-        # or indices[4] not in zIndices \
-        # or indices[5] not in zIndices
+indexPathMap = {}
 
-currentIndices = aIndices[2:3]
-count = 0
-leftRightLen = len(leftRightList)
-while notZ(currentIndices):
-    currentDirection = leftRightList[count % leftRightLen]
-    currentIndices = [indexMap[value][currentDirection] for value in currentIndices]
-    count += 1
+def findZ(lineIndex, leftRightIndex):
+    count = 0
+    while lineIndex not in zIndices:
+        count += 1
+        try:
+            nextDirection = leftRightStr[leftRightIndex]
+        except IndexError:
+            leftRightIndex = leftRightIndex % leftRightLen
+            nextDirection = leftRightStr[leftRightIndex]
+        lineIndex = indexMap[lineIndex][nextDirection]
+        leftRightIndex += 1
+    return count
 
-print(count)
+
+# don't ask me why this works (I think it's an accident)
+counts = [findZ(aIndex, 0) for aIndex in aIndices]
+print(np.lcm.reduce(counts))
+
+
+##### USELESS FUNCTION GRAVEYARD #####
+
+
+def followPath(startIndex, path):
+    if path in indexPathMap[startIndex]:
+        return indexPathMap[startIndex][path]
+    # TODO: potentially store subsets of path?
+    currentIndex = startIndex
+    for direction in path:
+        currentIndex = indexMap[currentIndex][direction]
+    indexPathMap[currentIndex] = path
+    return currentIndex
+
+def getNextNTurns(currentIndex, N):
+    newIndex = currentIndex + N
+    if newIndex <= leftRightLen:
+        # base case, no overflow
+        return newIndex, leftRightStr[currentIndex:newIndex]
+
+    # get rest of string before overflow
+    indexDiff = leftRightLen - currentIndex
+    currentStr = leftRightStr[currentIndex:]
+    newIndex -= indexDiff
+
+    # iterate until no overflow
+    while(newIndex > leftRightLen):
+        currentStr += leftRightStr
+        newIndex -= leftRightLen
+
+    # add the final piece and return
+    currentStr += leftRightStr[:newIndex]
+    return newIndex, currentStr
