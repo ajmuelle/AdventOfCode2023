a1e694e5245e6039f3e254d04e052f77e744ac32
Alex Mueller <ajmuelle@mtu.edu>
2023-12-17 22:05:20 -0500

    solves day 14 part 2 empirically

--- day14.py
+++ day14.py
@@ -1,6 +1,9 @@
+import copy
+import sys
+
 import numpy as np
 
-with open("input/day14tiny.txt") as day14:
+with open("input/day14.txt") as day14:
     lines = day14.readlines()
 
 numRows = len(lines)
@@ -10,6 +13,10 @@ numCols = len(lines[0][:-1])
 chars2d = []
 for line in lines:
     chars2d.append(list(line.replace("\n", "")))
+
+# convenience function for debugging
+def stringify():
+    return "\n".join(["".join(chars) for chars in chars2d])
 
 def findOpenRowNorth(row, col):
     while row > 0:
@@ -83,13 +90,49 @@ def rollEast():
 # rollNorth()
 
 ##### PART TWO #####
+
 def spinCycle():
     rollNorth()
     rollWest()
     rollSouth()
     rollEast()
 
-for x in range(1000000000):
+# printPoints = list(range(1000,1100))
+# stringCache = {}
+#
+# for x in range(1000000000):
+#     currentStr = stringify()
+#     if currentStr in stringCache:
+#         if x in printPoints:
+#             print(f"at iteration {x}, the string cache holds {stringCache[currentStr]}")
+#             # at iteration 1000, the string cache holds 98
+#             # at iteration 1001, the string cache holds 99
+#             # at iteration 1002, the string cache holds 100
+#             # at iteration 1003, the string cache holds 101
+#             # at iteration 1004, the string cache holds 102
+#             # at iteration 1005, the string cache holds 103
+#             # at iteration 1006, the string cache holds 104
+#             # at iteration 1007, the string cache holds 105
+#             # at iteration 1008, the string cache holds 106
+#             # at iteration 1009, the string cache holds 96
+#             # at iteration 1010, the string cache holds 97
+#             # at iteration 1011, the string cache holds 98
+#             # at iteration 1012, the string cache holds 99
+#             # at iteration 1013, the string cache holds 100
+#             # at iteration 1014, the string cache holds 101
+#             # at iteration 1015, the string cache holds 102
+#             # at iteration 1016, the string cache holds 103
+#             # at iteration 1017, the string cache holds 104
+#             # at iteration 1018, the string cache holds 105
+#             # at iteration 1019, the string cache holds 106
+#             # at iteration 1020, the string cache holds 96
+#     else:
+#         stringCache[currentStr] = x
+#     spinCycle()
+
+# used above code to prove that iteration N == iteration ((N - 8) mod 11) + 96
+numIterations = np.mod(1000000000 - 8, 11) + 96
+for x in range(numIterations):
     spinCycle()
 
 # count Os in each row
