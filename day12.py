d97078d673335e5de3f13c4ac065bdc6b73e2ff4
Alex J. Mueller <ajmuelle@mtu.edu>
2023-12-19 16:55:37 -0500

    day 12 naive brute force solution that doesn't halt on part 2

--- day12.py
+++ day12.py
@@ -1,36 +1,76 @@
-import numpy as np
-from scipy.special import comb
+import itertools
 
-with open("input/day12.txt") as day12:
+import numpy as np
+import scipy.special
+
+with open("input/day12tiny.txt") as day12:
     lines = day12.readlines()
 
-def partitionNumbers(segmentLengths, numbers):
-    # TODO: handle base cases
-    pass
 
-permCounts = []
-for line in lines:
-    sharpPart, numberPart = line.replace("\n", "").split(" ")
-    numbers = [int(number) for number in numberPart.split(",")]
-    dots, sharps, qMarks = [], [], []
+def unfold(sharpPart, numberPart):
+    newSharpPart = "?".join([sharpPart for x in range(5)])
+    newNumberPart = ",".join([numberPart for x in range(5)])
+    return newSharpPart, newNumberPart
+
+
+def isValid(lineStr, numbers):
+    sharpRuns = list(filter(None, lineStr.split(".")))
+    if len(sharpRuns) != len(numbers):
+        return False
+    for index, run in enumerate(sharpRuns):
+        if len(run) != numbers[index]:
+            return False
+    return True
+
+
+def countArrangements(sharpPart, numbers):
+    dots, sharps, qMarks = [], [], set()
     for index, char in enumerate(sharpPart):
         match char:
             case "#":
                 sharps.append(index)
             case "?":
-                qMarks.append(index)
+                qMarks.add(index)
             case _:
                 dots.append(index)
     correctSharpCount = np.sum(numbers)
     visibleSharpCount = len(sharps)
     missingSharpCount = correctSharpCount - visibleSharpCount
-    permCountUpperBound = comb(len(qMarks), missingSharpCount, exact=True)
+    arrangementsUpperBound = scipy.special.comb(len(qMarks), missingSharpCount, exact=True)
+    arrangements = 0
 
-    permCount = 1
-    segments = list(filter(lambda x: len(x) > 0, sharpPart.split(".")))
-    segmentLengths = [len(segment) for segment in segments]
-    partitions = partitionNumbers(segmentLengths, numbers)
+    # the set of all possible indices where a question mark is actually a sharp
+    missingSharpCombs = itertools.combinations(qMarks, missingSharpCount)
+    for comb in missingSharpCombs:
+        sharpIndices = set(comb)
+        dotIndices = qMarks.difference(sharpIndices)
+        lineArray = np.array(list(sharpPart))
+        lineArray[list(sharpIndices)] = "#"
+        lineArray[list(dotIndices)] = "."
+        lineStr = "".join(lineArray)
+        if isValid(lineStr, numbers):
+            arrangements += 1
+            # print(lineStr)
+
+    return arrangements
+
+
+# part 2 idea: construct list containing folded count followed by 4 instances
+# of what the count would be if you add a ? to the left, except when you know
+# for sure that folded ends with # (in day12tiny this works on the first 5
+# test cases but not the 6th, but 10 * 15 * 15 * 15 * 15 gets that answer)
+
+
+permCounts = []
+for line in lines:
+    sharpPart, numberPart = line.replace("\n", "").split(" ")
+    sharpPart, numberPart = unfold(sharpPart, numberPart)   # part 2 only
+    numbers = [int(number) for number in numberPart.split(",")]
+
+    permCount = countArrangements(sharpPart, numberPart)
+    permCounts.append(permCount)
 
     pass
 
+# the answer is undoubtedly less than 727925
 print(np.sum(permCounts))
