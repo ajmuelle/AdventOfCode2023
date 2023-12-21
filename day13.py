9cbe0bec2b22d1802f06ced4257303332c627216
Alex J. Mueller <ajmuelle@mtu.edu>
2023-12-21 15:30:18 -0500

    inching closer on day 13 part 2

--- day13.py
+++ day13.py
@@ -10,74 +10,96 @@ for line in lines:
         searchAreas.append(currentSearchArea)
         currentSearchArea = []
     else:
-        currentSearchArea.append(line.replace("\n",""))
+        currentSearchArea.append(list(line.replace("\n","")))
 searchAreas.append(currentSearchArea)
+
+
+class SmudgeException(Exception):
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
 
 def rowDiff(searchArea, row1, row2):
     diff = 0
+    smudgeIndex = 0
     for index in range(len(searchArea[0])):
         if searchArea[row1][index] != searchArea[row2][index]:
             diff += 1
+            smudgeIndex = index
+    if diff == 1:
+        raise SmudgeException(row1, smudgeIndex)
     return diff
 
 def colDiff(searchArea, col1, col2):
     diff = 0
+    smudgeIndex = 0
     for index in range(len(searchArea)):
         if searchArea[index][col1] != searchArea[index][col2]:
             diff += 1
+            smudgeIndex = index
+    if diff == 1:
+        raise SmudgeException(smudgeIndex, col1)
     return diff
 
 def columnsMatch(searchArea, column1, column2):
-    for line in searchArea:
-        if line[column1] != line[column2]:
-            return False
-    return True
+    return colDiff(searchArea, column1, column2) == 0
 
 def rowsMatch(searchArea, row1, row2):
-    return searchArea[row1] == searchArea[row2]
+    return rowDiff(searchArea, row1, row2) == 0
 
 def verifyColumnReflection(searchArea, column):
     lineLength = len(searchArea[0])
     left = column
     right = column + 1
-    smudge = False
+    numSmudges = 0
     while left >= 0 and right < lineLength:
-        if not columnsMatch(searchArea, left, right):
-            if colDiff(searchArea, left, right) == 1 and not smudge:
-                smudge = True
-            else:
+        try:
+            if not columnsMatch(searchArea, left, right):
                 return False
+        except SmudgeException as e:
+            numSmudges += 1
+            searchArea[e.row][e.col] = "O"
+            print(f"smudge found at row {e.row}, column {e.col}:")
+            for line in searchArea:
+                print("".join(line))
         left -= 1
         right += 1
-    return True
+    return numSmudges == 1
 
 def verifyRowReflection(searchArea, row):
     numRows = len(searchArea)
     top = row
     bottom = row + 1
-    smudge = False
+    numSmudges = 0
     while top >= 0 and bottom < numRows:
-        if not rowsMatch(searchArea, top, bottom):
-            if rowDiff(searchArea, top, bottom) == 1 and not smudge:
-                smudge = True
-            else:
+        try:
+            if not rowsMatch(searchArea, top, bottom):
                 return False
+        except SmudgeException as e:
+            numSmudges += 1
+            searchArea[e.row][e.col] = "O"
+            print(f"smudge found at row {e.row}, column {e.col}")
+            for line in searchArea:
+                print("".join(line))
         top -= 1
         bottom += 1
-    return True
+    return numSmudges == 1
 
 horizontalMirrors = []
 verticalMirrors = []
 
 for searchArea in searchAreas:
+    print("-----NEW SEARCH AREA-----")
     for index in range(len(searchArea) - 1):
         if verifyRowReflection(searchArea, index):
             horizontalMirrors.append(index + 1)
+            print(f"{index + 1} added to horizontal mirrors")
             break
     else:
         for index in range(len(searchArea[0]) - 1):
             if verifyColumnReflection(searchArea, index):
                 verticalMirrors.append(index + 1)
+                print(f"{index + 1} added to vertical mirrors")
                 break
 
 print(np.sum(verticalMirrors) + 100 * np.sum(horizontalMirrors))
@@ -86,4 +108,5 @@ print(np.sum(verticalMirrors) + 100 * np.sum(horizontalMirrors))
 
 # part 2
 # 9973 too low
+# 28518 too low
 # 34607 too high
